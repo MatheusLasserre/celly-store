829aa48f74fca0650f0bc6378caac2b9d9536ac0
Matheus Lasserre <matheuslasserre3@gmail.com>
2024-01-28 15:45:42 -0300

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { api } from "~/trpc/server";
 export default async function Home() {
   noStore();
   const hello = await api.post.hello.query({ text: "from tRPC" });
-// test
+// test2
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
